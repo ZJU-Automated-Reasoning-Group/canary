cdb32e6587ff508896dc0aefaccef01ecc205899
rainoftime <rainoftime@gmail.com>
2025-04-10 13:23:58 +0800

    add WPDS

---
+++ include/Solvers/WPDS/compat.h
@@ -0,0 +1,24 @@
+#ifndef WPDS_COMPAT_H
+#define WPDS_COMPAT_H
+
+// Compatibility shims for modern C++ standards
+// These definitions replace the deprecated std::unary_function and std::binary_function
+
+namespace std {
+    // Replacement for std::unary_function
+    template<typename Arg, typename Result>
+    struct unary_function {
+        typedef Arg argument_type;
+        typedef Result result_type;
+    };
+
+    // Replacement for std::binary_function
+    template<typename Arg1, typename Arg2, typename Result>
+    struct binary_function {
+        typedef Arg1 first_argument_type;
+        typedef Arg2 second_argument_type;
+        typedef Result result_type;
+    };
+}
+
+#endif // WPDS_COMPAT_H 
